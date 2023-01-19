2755ede305b60199a7acb0f8af03ff8e6d4aa38a
ramonbr14 <ramonbr14@gmail.com>
2023-01-18 23:23:03 -0400

    Trilha Fundamentar - Guia Estelar JavaScript - variaveis concluidas.

---
+++ TiposEcomentarios.js
@@ -0,0 +1,14 @@
+console.log('"Ramon"Santos"silva"')
+console.log(`Rainha Elsa ${1+20}`)
+console.log(23+34);
+console.log(false);
+console.log(undefined);
+console.log(null);
+
+console.log({
+    name: "Rainha Anna",
+    idade: 18,
+    andar: function(){
+        console.log('Pular');
+    }
+});
---
+++ script.js
@@ -0,0 +1,16 @@
+/*var clima = "Quente"
+let clima2 = "Frio"
+const clima3 = "Ameno";
+clima = "frio"
+console.log(clima);
+console.log(clima2);
+console.log(clima3);*/
+
+console.log('existe y antes do bloco?',y);
+
+        {
+            //var y = 0
+            let y = 0
+
+        }
+ console.log('existe y depois do bloco?',y);
